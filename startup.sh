659ed40c8b7a4a474e1a97911c062d36750a7f05
COGNET, JULIEN <julien.cognet@cgi.com>
2022-10-04 12:31:19 +0200

    Updates on startup

--- startup.sh
+++ startup.sh
@@ -10,11 +10,11 @@ cd ../..
 echo ""
 echo ">> Deploy Flink Stream Java Api Demo"
 docker exec flink-jobmanager sh -c "flink list -r | grep \"Flink job to \" | cut -f 4 -d \" \" | while read -r line ; do flink stop --savepointPath /tmp/savepoint $line; done"
-docker exec flink-jobmanager sh -c "flink run --detached /home/jars/flink-stream-api-demo-1.0-SNAPSHOT.jar"
+docker exec flink-jobmanager sh -c "flink run --detached /usr/local/flink/java_stream_api/flink-stream-api-demo-1.0-SNAPSHOT.jar"
 
 echo ""
 echo ">> Deploy Flink SQL Api Demo"
-docker exec -ti flink-jobmanager sh -c "sql-client.sh -f /usr/local/flink/createFlinkSqlJob.sql"
+docker exec -ti flink-jobmanager sh -c "sql-client.sh -f /usr/local/flink/table_api/createFlinkSqlJob.sql"
 
 echo ""
 echo ">> Generate Change Data Capture Records"
